ee5f2a03e23f333c817d1f7a5a247646dff9ada6
rozmarin <romaostafeychuk@gmail.com>
2026-03-08 20:15:28 +0300

    split parse() into action handlers with switch

--- lecture_hw_1/include/parser.hpp
+++ lecture_hw_1/include/parser.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "lexer.hpp"
+#include "slr_table.hpp"
 
 class Parser {
 public:
@@ -18,8 +19,17 @@ private:
         int state;
     };
 
+    using Stack = std::vector<StackEntry>;
+
     std::vector<Token> tokens_;
 
-    std::string stackToString(const std::vector<StackEntry>& stack) const;
+    void printTraceHeader() const;
+    void printTraceStep(const Stack& stack, std::size_t tokenPosition, Action action) const;
+
+    bool applyShift(const Token& lookahead, Action action, Stack& stack, std::size_t& tokenPosition) const;
+    bool applyReduce(Action action, Stack& stack) const;
+    bool handleError(const Token& lookahead, bool trace) const;
+
+    std::string stackToString(const Stack& stack) const;
     std::string inputToString(std::size_t tokenPosition) const;
 };
