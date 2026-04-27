63944f2515312a32ec8cc225b450493b34c71075
rozmarin <romaostafeychuk@gmail.com>
2026-04-27 16:53:17 +0300

    fix: define conv2d output size helper

--- lecture_hw_5_KERNELS/include/kernels/conv2d.hpp
+++ lecture_hw_5_KERNELS/include/kernels/conv2d.hpp
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <stdexcept>
+
 namespace kernels {
 
-int conv2d_output_size(int input_size, int kernel_size);
+inline int conv2d_output_size(int input_size, int kernel_size) {
+    if (input_size <= 0) {
+        throw std::invalid_argument("input_size");
+    }
+    if (kernel_size <= 0) {
+        throw std::invalid_argument("kernel_size");
+    }
+    if (kernel_size > input_size) {
+        throw std::invalid_argument("kernel_size");
+    }
+    return input_size - kernel_size + 1;
+}
 
 }  // namespace kernels
